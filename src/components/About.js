561e2a4f7558d50703d69eaef76f0fcbafef6e55
clarky <clark@dnamicro.com>
2020-02-06 06:43:10 +0800

    import reactmd and materialIcns locally

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,19 @@
 import React from 'react'
+import { Paper } from 'react-md'
 
 const About = () => (
-  <span>About</span>
+  // <span>About</span>
+  <div className='md-grid' style={{justifyContent: 'center'}}>
+    <div className='md-cell--4'>
+      <Paper
+        key='paper'
+        zDepth={3}
+        className="papers__example"
+      >
+        <span>About</span>
+      </Paper>
+    </div>
+  </div>
 )
 
 export default About

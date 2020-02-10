7bddd2f46bdd11742dc5b08d49fd59e21a811ef6
darwayne <darwayne@dnamicro.net>
2020-02-10 13:36:16 +0800

    imported lodash for auto formatting of labels on TextFields

--- src/utils/textFieldMapper.js
+++ src/utils/textFieldMapper.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import { TextField } from 'react-md'
+import toLower from 'lodash/toLower'
+import startCase from 'lodash/startCase'
 
 const textFieldMapper = (obj, keys) => {
   return keys.map(key => {
     const val = obj[key]
+    const label = key === 'dob'
+      ? 'Date of Birth'
+      : startCase(toLower(startCase(key)))
     return (
       <TextField
         disabled
         key={key}
-        label={`${key}: `}
+        label={`${label}: `}
         id={key}
         value={val || 'none'}
         className="md-cell md-cell--4"
